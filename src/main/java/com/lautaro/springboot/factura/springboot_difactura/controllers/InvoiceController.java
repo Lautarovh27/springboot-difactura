ec182b07cae005c6ba252d0729ce9daad6567e2b
Lautaro <lautarovanh@gmail.com>
2026-06-03 12:17:39 -0300

    Contectos de scope de los componentes, falta finalizar

--- src/main/java/com/lautaro/springboot/factura/springboot_difactura/controllers/InvoiceController.java
+++ src/main/java/com/lautaro/springboot/factura/springboot_difactura/controllers/InvoiceController.java
@@ -1,6 +1,7 @@
 package com.lautaro.springboot.factura.springboot_difactura.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.lautaro.springboot.factura.springboot_difactura.models.Invoice;
@@ -12,7 +13,7 @@ public class InvoiceController {
     @Autowired
     private Invoice invoice;
 
-    @RequestMapping("/show")
+    @GetMapping("/show")
     public Invoice show(){
         return invoice;
     }
--- src/main/java/com/lautaro/springboot/factura/springboot_difactura/models/Invoice.java
+++ src/main/java/com/lautaro/springboot/factura/springboot_difactura/models/Invoice.java
@@ -5,11 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
 @Component
+@RequestScope
+@JsonIgnoreProperties({"targetSource"})
 public class Invoice {
     
     @Autowired
